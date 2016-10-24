c6a5a67d722818ba3b08ec62fc818bcf34840708
mjvolk <michaelvolk@utexas.edu>
2016-10-24 09:41:29 -0500

    Reading questions from firebase and displaying text in Table View

---
+++ Geopoll/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  Geopoll
+//
+//  Created by Michael Volk on 10/23/16.
+//  Copyright © 2016 cs378Group4. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
