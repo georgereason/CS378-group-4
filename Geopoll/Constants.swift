f0dcbc888e59a8902b4e0d98295a7e748c91447f
mjvolk <michaelvolk@utexas.edu>
2016-10-25 23:49:13 -0500

    Adding new polls to firebase

---
+++ Geopoll/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Geopoll
+//
+//  Created by Michael Volk on 10/25/16.
+//  Copyright © 2016 cs378Group4. All rights reserved.
+//
+
+import Foundation
+
+struct USER {
+    var providerID:String
+    var uid:String
+    var name:String
+    var email:String
+}
+
+var CURRENT_USER:USER? = nil
